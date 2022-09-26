5cef34e4b77f88fd12c71b496c559e382575c098
Pramila -S <pramila18111993@gmail.com>
2022-09-26 21:47:20 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css"
+import './sb-admin-2.css';
 import './App.css';
+import SideBar from "./sideNav.jsx";
+import NavBar from "./navBar";
+import Dashboard from "./Dashboard";
+import Footer from "./Footer";
+import Users from "./Users";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./Product";
+import Newuser from './Newuser'
+import Userview from "./Userview";
+import Edit from "./Edit";
+import EditProduct from "./EditProduct";
+import ViewProduct from "./ViewProduct";
+import CreateProduct from "./CreateProduct";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <div id="page-top">
+          <div id="wrapper">
+            <SideBar></SideBar>
+            <div id="content-wrapper" className="d-flex flex-column">
+              <div id="content">
+                <NavBar></NavBar>
+                <div className="container-fluid">
+
+                  <Routes>
+
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/Dashboard" element={<Dashboard />} />
+                    <Route path="/Users" element={<Users />} />
+                    <Route path="Users/Newuser" element={<Newuser />} />
+                    <Route path="Users/:id" element={<Userview />} />
+                    <Route path="Users/edit/:id" element={<Edit />} />
+
+                    <Route path="/Product" element={<Product />}/>
+                    <Route path="Product/create" element={<CreateProduct/>}/>
+                    <Route path="Product/View/:id" element={<ViewProduct/>}/>
+                    <Route path="product/edit/:id" element={<EditProduct/>}/>
+                  </Routes>
+                </div>
+              </div>
+              <Footer></Footer>
+            </div>
+          </div>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
